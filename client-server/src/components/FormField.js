aea26db1e1353225e8029c58cac919247a0b6125
ircamitoc <iraincamitoc@gmail.com>
2023-09-11 23:29:08 +0800

    Updated FormField.js

--- client-server/src/components/FormField.js
+++ client-server/src/components/FormField.js
@@ -1,47 +1,24 @@
-import React, { useState } from "react";
+import React, { forwardRef } from "react";
 
-function FormField({ label, name, value, onChange }) {
-  // State to track whether the input is focused or has content
-  const [isFocused, setIsFocused] = useState(false);
-
-  // Function to handle input focus
-  const handleInputFocus = () => {
-    setIsFocused(true);
-  };
-
-  const handleInputBlur = () => {
-    setIsFocused(false);
-  };
-
-  // Function to handle input change
-  const handleInputChange = (e) => {
-    onChange(e); // Propagate the change event
-  };
-
-  return (
-    <div
-      className={`input-wrap w-100 ${
-        isFocused || value ? "not-empty focus" : ""
-      }`}
-    >
-      <input
-        className="contact-input"
-        autoComplete="off"
-        name={name}
-        type="text"
-        onChange={handleInputChange}
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-        value={value}
-      />
-      <label>{label}</label>
-      <i
-        className={`icon fa-solid fa-${
-          name === "name" ? "address-card" : "envelope"
-        }`}
-      ></i>
-    </div>
-  );
-}
+const FormField = forwardRef(
+  ({ label, name, value, onChange, onFocus, onBlur }, ref) => {
+    return (
+      <div className="input-wrap">
+        <input
+          type="text"
+          className="contact-input"
+          name={name}
+          value={value}
+          onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          ref={ref} // Pass the ref here
+        />
+        <label>{label}</label>
+        <i className="icon fa-solid fa-inbox"></i>
+      </div>
+    );
+  }
+);
 
 export default FormField;
